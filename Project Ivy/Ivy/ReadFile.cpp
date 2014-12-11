f7c7a5039a246e719ce431600836a283d0e4f622
Tim van de Burgt <timvandeburgt@hotmail.com>
2014-12-11 11:29:36 +0100

    readFile function

--- Project Ivy/Ivy/ReadFile.cpp
+++ Project Ivy/Ivy/ReadFile.cpp
@@ -1,8 +1,21 @@
 #include "ReadFile.h"
+#include <fstream>
+#include <iostream>
+#include <string>
 
 void ReadFile::Execute(std::vector<boost::any> arglist)
 {
-
+	//D:\\local\\test.txt"
+	std::string filepath = boost::any_cast<std::string>(arglist[0]);
+	std::ifstream file;
+	file.open(filepath);
+	std::string tempstring;
+	std::string resultstring;
+	while (std::getline(file, tempstring))
+	{
+		resultstring += tempstring + " ";
+	}
+	Result = resultstring;
 }
 
 boost::any ReadFile::GetResult()
