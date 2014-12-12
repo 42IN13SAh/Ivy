7eaa3ae93b2ac37c5c235f76be6a097fbbe87675
nvanson <n.vanson@hotmail.nl>
2014-12-12 16:06:17 +0100

    Mathfunctions-met-Math.h-geimplementeerd

--- Project Ivy/Ivy/Pow.cpp
+++ Project Ivy/Ivy/Pow.cpp
@@ -4,10 +4,7 @@ void Pow::Execute(std::vector<boost::any> arglist)
 {
 	double a = boost::any_cast<double>(arglist[0]);
 	double b = boost::any_cast<double>(arglist[1]);
-	double tempa = a;
-	for (int i = 0; i < b; i++)
-		a = a * tempa;
-	Result = a;
+	Result = std::pow(a, b);
 }
 
 boost::any Pow::GetResult()
